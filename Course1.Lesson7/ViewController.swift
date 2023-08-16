0355bd1089f67ae3564cfa72ededcd33210c0ad0
elpablo <elpablo.volgograd@yandex.ru>
2023-08-16 09:38:16 +0300

    Initial Commit

---
+++ Course1.Lesson7/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Course1.Lesson7
+//
+//  Created by Павел on 16.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
