9923ba07cf2b78b6f8899587c9b83fb4d75e29ee
elpablo <elpablo.volgograd@yandex.ru>
2023-08-29 15:18:12 +0300

    pod

---
+++ Pods/Target Support Files/Pods-Course1.Lesson7-Course1.Lesson7UITests/Pods-Course1.Lesson7-Course1.Lesson7UITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Course1_Lesson7_Course1_Lesson7UITests : NSObject
+@end
+@implementation PodsDummy_Pods_Course1_Lesson7_Course1_Lesson7UITests
+@end
---
+++ Pods/Target Support Files/Pods-Course1.Lesson7Tests/Pods-Course1.Lesson7Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Course1_Lesson7Tests : NSObject
+@end
+@implementation PodsDummy_Pods_Course1_Lesson7Tests
+@end
